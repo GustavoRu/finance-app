e256232a72875264278b0bd49b68eb902564cdaa
GustavoRu <gustavoruiz.gr96@gmail.com>
2024-11-06 01:15:30 -0300

    cambio en .sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 echo "Running composer"
 # composer global require hirak/prestissimo
+composer global remove hirak/prestissimo
+composer self-update
+echo "PASE EL UPDATE"
 composer install --no-dev --working-dir=/var/www/html
 echo "Caching config..."
 php artisan config:cache
